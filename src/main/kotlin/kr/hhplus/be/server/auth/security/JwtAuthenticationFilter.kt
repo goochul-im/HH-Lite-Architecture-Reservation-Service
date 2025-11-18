de50f7bea43aa4f13dd58be79382f10e1b8eff12
goochul-im <goochul175465@gmail.com>
2025-11-18 23:30:30 +0900

    refactor(auth): 토큰 검사 통과 경로 추가 및 리팩토링

--- src/main/kotlin/kr/hhplus/be/server/auth/security/JwtAuthenticationFilter.kt
+++ src/main/kotlin/kr/hhplus/be/server/auth/security/JwtAuthenticationFilter.kt
@@ -3,9 +3,11 @@ package kr.hhplus.be.server.auth.security
 import jakarta.servlet.FilterChain
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
+import kr.hhplus.be.server.application.wating.port.WaitingQueuePort
 import kr.hhplus.be.server.auth.UserStatus
 import kr.hhplus.be.server.auth.exception.AccessDeniedException
 import kr.hhplus.be.server.common.jwt.JwtProvider
+import mu.KotlinLogging
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.stereotype.Component
@@ -16,12 +18,29 @@ import org.springframework.web.filter.OncePerRequestFilter
 class JwtAuthenticationFilter(
     private val jwtProvider: JwtProvider,
     private val userDetailsService: CustomUserDetailsService,
+    private val waitingQueuePort: WaitingQueuePort
 ) : OncePerRequestFilter() {
+
+    private val log = KotlinLogging.logger { }
+    private val matcher = AntPathMatcher()
+
 
     companion object {
         private const val ACCESS_TOKEN_HEADER = "Authorization"
         private const val WAITING_TOKEN_HEADER = "X-Waiting-Token"
         private const val TOKEN_PREFIX = "Bearer "
+        private val EXCLUDED_PATHS = listOf(
+            "/api/auth/**",
+        )
+        private val NO_WAITING_TOKEN_PATH = listOf(
+            "/api/pay/**",
+            "/api/wait/enter"
+        )
+    }
+
+    override fun shouldNotFilter(request: HttpServletRequest): Boolean {
+        val path = request.servletPath
+        return EXCLUDED_PATHS.any { matcher.match(it, path) }
     }
 
     override fun doFilterInternal(
@@ -32,13 +51,6 @@ class JwtAuthenticationFilter(
         val accessToken = resolveToken(request, ACCESS_TOKEN_HEADER)
         val waitingToken = resolveToken(request, WAITING_TOKEN_HEADER)
         val path = request.servletPath
-        val matcher = AntPathMatcher()
-
-        // 토큰이 필요 없는 경로 (로그인, 회원가입 등)
-        if (matcher.match("/api/auth/**", path)) {
-            filterChain.doFilter(request, response)
-            return
-        }
 
         try {
             // 1. Access Token 검증 (대기열 진입 API 포함 모든 인증 필요 API)
@@ -47,23 +59,18 @@ class JwtAuthenticationFilter(
             }
             setAuthentication(accessToken) // 인증 정보 설정
 
-            // 2. Waiting Token 검증 (대기열 진입 API를 제외한 나머지)
-            val requiresActiveWaitingToken = !matcher.match("/api/wait/enter", path)
+            // 2. Waiting Token 검증
+            val requiresActiveWaitingToken = !NO_WAITING_TOKEN_PATH.any{ matcher.match(it, path) }
 
             if (requiresActiveWaitingToken) {
-                if (waitingToken == null || !jwtProvider.validateToken(waitingToken)) {
+                //현재 접속 리스트에 존재하는지
+                val accessUserId = jwtProvider.getUserIdFromToken(accessToken)
+                log.info { "accessUserId = $accessUserId, waitingToken = $waitingToken" }
+                if (waitingToken == null || !waitingQueuePort.isEnteringKey(accessUserId)) {
                     throw AccessDeniedException("Waiting token is required or invalid.")
                 }
-
-                // Waiting Token의 상태가 ACTIVE인지 확인
-                val status = jwtProvider.getStatusFromToken(waitingToken)
-                if (status != UserStatus.ACTIVE) {
-                    throw AccessDeniedException("User is not in ACTIVE status.")
-                }
-
-                // 두 토큰의 사용자 ID가 일치하는지 확인
-                val accessUserId = jwtProvider.getUserIdFromToken(accessToken)
                 val waitingUserId = jwtProvider.getUserIdFromToken(waitingToken)
+                // 두 토큰의 사용자 ID가 일치하는지 확인
                 if (accessUserId != waitingUserId) {
                     throw AccessDeniedException("Token user ID mismatch.")
                 }
