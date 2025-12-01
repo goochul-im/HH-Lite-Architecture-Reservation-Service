018583f9f21e7a9094fea6dd3473893a95502fa7
goochul-im <goochul175465@gmail.com>
2025-12-02 00:04:17 +0900

    fix(Member): 회원가입 시 비밀번호 인코딩되도록 수정

--- src/main/kotlin/kr/hhplus/be/server/member/service/MemberService.kt
+++ src/main/kotlin/kr/hhplus/be/server/member/service/MemberService.kt
@@ -2,17 +2,19 @@ package kr.hhplus.be.server.member.service
 
 import kr.hhplus.be.server.member.domain.Member
 import kr.hhplus.be.server.member.port.MemberRepository
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
 
 @Service
 class MemberService(
-    private val memberRepository: MemberRepository
+    private val memberRepository: MemberRepository,
+    private val passwordEncoder: PasswordEncoder
 ) {
 
     fun signUp(username: String, password: String) {
         val member = Member(
             username = username,
-            password = password
+            password = passwordEncoder.encode(password)
         )
         memberRepository.save(member)
     }
