e0810477e75e5702a5586fd3a495cb2baa101c02
goochul-im <goochul175465@gmail.com>
2025-12-02 01:02:15 +0900

    refactor(Member): 외부 모듈을 헥사고날 아키텍처를 사용하여 분리
    PasswordEncoder를 분리하여 테스트하기 쉽도록 함

--- src/main/kotlin/kr/hhplus/be/server/member/service/MemberService.kt
+++ src/main/kotlin/kr/hhplus/be/server/member/service/MemberService.kt
@@ -1,22 +1,22 @@
 package kr.hhplus.be.server.member.service
 
 import kr.hhplus.be.server.member.domain.Member
+import kr.hhplus.be.server.member.port.EncodeService
 import kr.hhplus.be.server.member.port.MemberRepository
-import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
 
 @Service
 class MemberService(
     private val memberRepository: MemberRepository,
-    private val passwordEncoder: PasswordEncoder
+    private val passwordEncoder: EncodeService
 ) {
 
-    fun signUp(username: String, password: String) {
+    fun signUp(username: String, password: String) : Member {
         val member = Member(
             username = username,
             password = passwordEncoder.encode(password)
         )
-        memberRepository.signUp(member)
+        return memberRepository.signUp(member)
     }
 
 }
