7e43b519f63a0b3e4e4ac3bef4e7d20b2b3bcd07
goochul-im <goochul175465@gmail.com>
2025-12-04 01:55:59 +0900

    refactor(Reservation): ReservationService에서 아웃박스 패턴 사용하도록 수정

--- src/main/kotlin/kr/hhplus/be/server/reservation/service/ReservationService.kt
+++ src/main/kotlin/kr/hhplus/be/server/reservation/service/ReservationService.kt
@@ -3,10 +3,16 @@ package kr.hhplus.be.server.reservation.service
 import jakarta.transaction.Transactional
 import kr.hhplus.be.server.member.infrastructure.MemberEntity
 import kr.hhplus.be.server.member.port.MemberRepository
+import kr.hhplus.be.server.outbox.domain.AggregateType
+import kr.hhplus.be.server.outbox.domain.EventType
+import kr.hhplus.be.server.outbox.domain.OutboxMessage
+import kr.hhplus.be.server.outbox.domain.OutboxStatus
+import kr.hhplus.be.server.outbox.port.OutboxRepository
 import kr.hhplus.be.server.reservation.domain.Reservation
 import kr.hhplus.be.server.reservation.infrastructure.ReservationEntity
 import kr.hhplus.be.server.reservation.infrastructure.ReservationStatus
 import kr.hhplus.be.server.reservation.dto.ReservationRequest
+import kr.hhplus.be.server.reservation.dto.TempReservationPayload
 import kr.hhplus.be.server.reservation.port.ReservationRepository
 import kr.hhplus.be.server.reservation.port.TempReservationPort
 import org.springframework.beans.factory.annotation.Value
@@ -20,8 +26,10 @@ class ReservationService(
     private val memberRepository: MemberRepository,
     @param:Value("\${reservation.price}")
     private val price: Int,
+    private val outboxRepository: OutboxRepository
 ) {
 
+    @Transactional
     fun make(dto: ReservationRequest) : Reservation {
 
         if (!getAvailableSeat(dto.date).contains(dto.seatNumber)) {
@@ -36,7 +44,15 @@ class ReservationService(
         )
 
         val save : Reservation = reservationRepository.save(reservation)
-        tempReservationService.save(dto.date, save.id!!, reservation.seatNumber)
+
+        val outboxMessage = OutboxMessage(
+            aggregateType = AggregateType.TEMP_RESERVATION,
+            eventType = EventType.INSERT,
+            payload = TempReservationPayload(save.id!!, save.date, save.seatNumber).toMap(),
+            status = OutboxStatus.PENDING
+        )
+
+        outboxRepository.save(outboxMessage)
         return save
     }
 
