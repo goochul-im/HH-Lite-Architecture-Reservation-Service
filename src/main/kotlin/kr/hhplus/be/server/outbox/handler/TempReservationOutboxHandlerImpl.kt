631900974140b588613a0c7e6631eab676283b34
goochul-im <goochul175465@gmail.com>
2025-12-04 22:39:39 +0900

    refactor(TempReservation): 아웃박스 핸들러에서 트랜잭션 삭제
    트랜잭션이 상위 스케줄러에서 걸려야 다같이 롤백되기에 수정. 이전에는 스케줄러에서 아웃박스에 저장한다는 가정이 있었음

--- src/main/kotlin/kr/hhplus/be/server/outbox/handler/TempReservationOutboxHandlerImpl.kt
+++ src/main/kotlin/kr/hhplus/be/server/outbox/handler/TempReservationOutboxHandlerImpl.kt
@@ -32,12 +32,6 @@ class TempReservationOutboxHandlerImpl(
         return aggregateType == AggregateType.TEMP_RESERVATION
     }
 
-    /**
-     * 새로운 트랜잭션에서 시작합니다. 같은 트랜잭션 내에서 시작할 경우 아웃박스도
-     * 롤백되어 삭제되는 경우가 생길 수 있습니다.
-     * outboxRepository에 DONE 상태로 업데이트합니다.
-     */
-    @Transactional(propagation = Propagation.REQUIRES_NEW)
     override fun handle(message: OutboxMessage): OutboxMessage {
         val payload = message.payload
         try {
