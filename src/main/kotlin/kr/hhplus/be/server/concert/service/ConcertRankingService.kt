fa1939388bb4cb18477a3e34d3fb8137b8140622
goochul-im <goochul175465@gmail.com>
2026-01-20 00:21:16 +0900

    in-progress(Concert)

---
+++ src/main/kotlin/kr/hhplus/be/server/concert/service/ConcertRankingService.kt
@@ -0,0 +1,23 @@
+package kr.hhplus.be.server.concert.service
+
+import kr.hhplus.be.server.concert.port.ConcertRepository
+import kr.hhplus.be.server.concert.port.ConcertSoldOutPort
+import org.springframework.stereotype.Service
+
+@Service
+class ConcertRankingService(
+    private val soldOutPort: ConcertSoldOutPort,
+    private val concertRepository: ConcertRepository
+) {
+
+    fun checkAndMarkSoldOut(concertId: Long) {
+        if (soldOutPort.isSoldOut(concertId)) {
+            return
+        }
+
+        val concert = concertRepository.findById(concertId)
+        soldOutPort.markSoldOut(concertId, System.currentTimeMillis())
+
+    }
+
+}
