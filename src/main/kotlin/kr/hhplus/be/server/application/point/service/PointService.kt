97ff04b416107ac3ed09b3effa454d7a1fe9dfb7
goochul-im <goochul175465@gmail.com>
2026-01-29 21:45:29 +0900

    feat(event): 이벤트 발행 로직 작성

--- src/main/kotlin/kr/hhplus/be/server/application/point/service/PointService.kt
+++ src/main/kotlin/kr/hhplus/be/server/application/point/service/PointService.kt
@@ -1,18 +1,27 @@
 package kr.hhplus.be.server.application.point.service
 
-import kr.hhplus.be.server.member.infrastructure.MemberJpaRepository
+import jakarta.transaction.Transactional
+import kr.hhplus.be.server.application.point.event.PointChargedEvent
+import kr.hhplus.be.server.common.event.DomainEventPublisher
 import kr.hhplus.be.server.member.port.MemberRepository
 import org.springframework.stereotype.Service
 
 @Service
 class PointService(
     private val memberRepository: MemberRepository,
+    private val eventPublisher: DomainEventPublisher
 ) {
 
+    @Transactional
     fun charge(id: String, chargePoint: Int) : Int {
         val member = memberRepository.findById(id)
         member.chargePoint(chargePoint)
         memberRepository.save(member)
+        eventPublisher.publish(PointChargedEvent(
+            member.id!!,
+            chargePoint,
+            member.point,
+        ))
         return member.point
     }
 
